1ef12ddd05fc5aefbc43492b7ff2bd6988eaaa27
sandipsawant10 <sandipsawant503@gmail.com>
2025-12-22 22:18:37 +0530

    Fix response handling in addRecipe and improve formatting in recipe routes

--- backend/controller/recipe.js
+++ backend/controller/recipe.js
@@ -13,7 +13,7 @@ const addRecipe = async (req, res) => {
   const { title, ingredients, instructions, time } = req.body;
 
   if (!title || !ingredients || !instructions) {
-    res.json({ message: "Please fill all required fields" });
+    return res.status(400).json({ message: "Please fill all required fields" });
   }
 
   const newRecipe = await Recipe.create({
--- backend/routes/recipe.js
+++ backend/routes/recipe.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const {
   getRecipes,
+  getRecipe,
   addRecipe,
   editRecipe,
   deleteRecipe,
 } = require("../controller/recipe");
 const router = express.Router();
 
-router.get("/", getRecipes); //Get all recipes
-router.get("/:id", getRecipes); //Get recipe by ID
-router.post("/:id", addRecipe); //Add a new recipe
-router.put("/:id", editRecipe); //Edit a recipe
-router.delete("/:id", deleteRecipe); //Delete a recipe
+router.get("/", getRecipes); // Get all recipes
+router.get("/:id", getRecipe); // Get recipe by ID
+router.post("/", addRecipe); // Add a new recipe
+router.put("/:id", editRecipe); // Edit a recipe
+router.delete("/:id", deleteRecipe); // Delete a recipe
 
 module.exports = router;
