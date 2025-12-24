dc0925f101adfa62017a4cc896487f79b8c361c6
sandipsawant10 <sandipsawant503@gmail.com>
2025-12-24 07:07:46 +0530

    feat: implement user authentication with signup and login functionality

--- frontend/food-blog-app/src/App.jsx
+++ frontend/food-blog-app/src/App.jsx
@@ -17,7 +17,11 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <MainNavigation />,
-    children: [{ path: "/", element: <Home />, loader: getAllRecipes }],
+    children: [
+      { path: "/", element: <Home />, loader: getAllRecipes },
+      { path: "/myRecipe", element: <Home /> },
+      { path: "/favRecipe", element: <Home /> },
+    ],
   },
 ]);
 
--- frontend/food-blog-app/src/components/Navbar.jsx
+++ frontend/food-blog-app/src/components/Navbar.jsx
@@ -1,17 +1,51 @@
-import React from "react";
-
+import React, { useState } from "react";
+import InputForm from "./InputForm";
+import { set } from "mongoose";
+import { useEffect } from "react";
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  let token = localStorage.getItem("token");
+  const [isLogin, setIsLogin] = useState(token ? false : true);
+
+  useEffect(() => {
+    setIsLogin(token ? false : true);
+  }, [token]);
+
+  const checkLogin = () => {
+    if (token) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      setIsLogin(true);
+    } else {
+      setIsOpen(true);
+    }
+    setIsOpen(true);
+  };
   return (
     <>
       <header>
         <h2>Food Blog</h2>
         <ul>
-          <li>Home</li>
-          <li>My Recipe</li>
-          <li>Favourites</li>
-          <li>Login</li>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li onClick={() => isLogin && setIsLogin(true)}>
+            <NavLink to={!isLogin ? "/myRecipe" : "/"}>My Recipe</NavLink>
+          </li>
+          <li onClick={() => isLogin && setIsLogin(true)}>
+            <NavLink to={!isLogin ? "/favRecipe" : "/"}>Favourites</NavLink>
+          </li>
+          <li onClick={checkLogin}>
+            <p className="login">{isLogin ? "Login" : "Logout"}</p>
+          </li>
         </ul>
       </header>
+      {isOpen && (
+        <Modal onClose={() => setIsOpen(false)}>
+          {" "}
+          <InputForm setIsOpen={() => setIsOpen(false)} />
+        </Modal>
+      )}
     </>
   );
 }
